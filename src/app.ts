55345cdfb6a81e3cfbbc83734121fb5c307c3392
Denis Ivanov <site.ivanov2014@yandex.ru>
2024-04-10 14:07:07 +0300

    321

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,8 @@
 import express from 'express';
-import {
-    Response,
-    NextFunction,
-} from 'express';
-import { RequestCustom } from '../utils/types';
-
-const { PORT = 3002 } = process.env;
+import mongoose from 'mongoose';
 
 const app = express();
 
-// Здесь данные
-const users = [
-    { name: 'Мария', age: 22 },
-    { name: 'Виктор', age: 30 },
-    { name: 'Анастасия', age: 48 },
-    { name: 'Алексей', age: 51 }
-];
+mongoose.connect('mongodb://localhost:27017/printridgedb');
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
-});
-
-app.get('/users/:id/albums/:album/:photo', (req: RequestCustom, res: Response) => {
-    const { id, album, photo } = req.params;
-    res.send(`Мы на странице пользователя с id ${id}, смотрим альбом №${album} и фотографию №${photo}`);
-}); 
+app.listen(3000); 
